74207aa32d1326f1bb64cc1228fcd854b462fd8a
lavau <2723986904@qq.com>
2021-02-26 12:18:42 +0800

    2021/2：26 12：18

--- src/chapter2/interview/question3/Problem1.java
+++ src/chapter2/interview/question3/Problem1.java
@@ -1,4 +1,4 @@
-package chapter2;
+package chapter2.interview.question3;
 
 import org.junit.Test;
 
@@ -6,11 +6,11 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- * 题目：找出数组中重复的数字。
+ * 题目一：找出数组中重复的数字。
  * 在一个长度为n的数组里的所有数字都在0～n-1的范围内。数组中某些数字是重复的,但不知道有几个数字重复了,也不知道每个数字重复了几次。
  * 请找出数组中任意一个重复的数字。例如，如果输入长度为7的数组{2,3,1.0.2,5,3]，那么对应的输出是重复的数字2或者3.
  **/
-public class QuestionOfInterview3 {
+public class Problem1 {
     final int LENGTH = 7;
 
     /**
@@ -71,3 +71,4 @@ public class QuestionOfInterview3 {
         return nums;
     }
 }
+
